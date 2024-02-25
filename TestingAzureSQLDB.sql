4e475c13a4fdec84ebe39f3470e577d835ac80f9
RaposoManolo <47564554+RaposoManolo@users.noreply.github.com>
2024-02-25 12:02:20 +0000

    DB Changes

--- TestingAzureSQLDB.sql
+++ TestingAzureSQLDB.sql
@@ -1,1 +1,13 @@
-select * from dbo.TESTIMR
+
+/*
+Grating access to the newly created function - SIPPValue
+CREATE USER "SIPPValue" from EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER "SIPPValue"
+ALTER ROLE db_datawriter ADD MEMBER "SIPPValue"*/
+
+
+CREATE TABLE dbo.SippValue (
+  TrackID   INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+  TrackDate datetime DEFAULT(getdate()),
+  SippValue  SMALLMONEY NOT NULL
+)
